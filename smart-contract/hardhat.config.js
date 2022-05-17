c1cf04639bab88cc8807b59c3e1cd66b6f7cac1b
Jawad Arshad <56083056+HamJaw1432@users.noreply.github.com>
2022-05-17 09:17:55 -0400

    update config

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -5,13 +5,13 @@
 require("@nomiclabs/hardhat-waffle");
 // Go to https://www.alchemyapi.io, sign up, create
 // a new App in its dashboard, and replace "KEY" with its key
-const ALCHEMY_API_KEY = "81dTK_i66LTrIpGocWPJJEMIwsxvso6R";
+const ALCHEMY_API_KEY = "";
 
 // Replace this private key with your Ropsten account private key
 // To export your private key from Metamask, open Metamask and
 // go to Account Details > Export Private Key
 // Be aware of NEVER putting real Ether into testing accounts
-const ROPSTEN_PRIVATE_KEY = "a5ca36b266c2e528e12527cf3b07803edcfcda21d64f8ec4e09c71cf56d5ceb8";
+const ROPSTEN_PRIVATE_KEY = "";
 module.exports = {
   solidity: "0.8.0",
   networks: {
